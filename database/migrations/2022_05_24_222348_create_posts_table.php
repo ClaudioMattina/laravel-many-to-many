57687433dc313a62b665993613972bd1d2b5196b
CLaudioBruni <claudio.bruni95@hotmail.com>
2022-05-25 00:33:23 +0200

    migration di posts e categories

--- database/migrations/2022_05_24_222348_create_posts_table.php
+++ database/migrations/2022_05_24_222348_create_posts_table.php
@@ -15,6 +15,11 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('content');
+            $table->text('image_url');
+            $table->date('date');
+            $table->string('title');
             $table->timestamps();
         });
     }
